6a2d41c80e85a2e802c09a5863aeee137d030f6c
Andrei Doe <andreidoe@Andreis-MacBook.local>
2017-07-10 17:21:18 +0300

    added migrations for database (fixed)

--- database/migrations/2017_07_10_132353_create_messages_table.php
+++ database/migrations/2017_07_10_132353_create_messages_table.php
@@ -13,7 +13,7 @@ class CreateMessagesTable extends Migration
      */
     public function up()
     {
-            Schema::create('users', function (Blueprint $table) {
+            Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('username');
             $table->dateTime('date');
