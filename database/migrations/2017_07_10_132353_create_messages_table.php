5f00cd2a24e058599b5adeca6ca3a335fea31aab
Andrei Doe <andreidoe@Andreis-MacBook.local>
2017-07-10 17:13:46 +0300

    added migrations for database

---
+++ database/migrations/2017_07_10_132353_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+            Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->dateTime('date');
+            $table->text('text');
+            $table->integer('chat_id');
+            $table->integer('user_id');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
