5f3e8ddb5b80d24d88eaa1bc876e49076d9a9717
Andrei <pfffok@gmail.com>
2017-07-10 18:32:18 +0300

    fixed migrations...again

--- database/migrations/2017_07_10_132353_create_messages_table.php
+++ database/migrations/2017_07_10_132353_create_messages_table.php
@@ -14,14 +14,18 @@ class CreateMessagesTable extends Migration
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
-        $table->increments('id');
-        $table->string('username');
-        $table->dateTime('date');
-        $table->text('text');
-        $table->integer('chat_id');
-        $table->integer('user_id');
-        $table->rememberToken();
-        $table->timestamps();
+            $table->increments('id');
+            $table->string('username');
+            $table->dateTime('date');
+            $table->text('text');
+            $table->integer('chat_id');
+            $table->integer('user_id');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::table('messages', function($table) {
+            $table->foreign('chat_id')->references('id')->on('chats');
         });
     }
 
