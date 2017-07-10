e1e2382ed32ab4fa2aafd96460704c798557bb2f
Andrei Doe <andreidoe@Andreis-MacBook.local>
2017-07-10 16:08:58 +0300

    adding ClearDB support

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
 return [
 
     /*
@@ -13,12 +18,7 @@ return [
     |
     */
 
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
+    
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
@@ -63,13 +63,13 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-        'host'      => $host,
-        'database'  => $database,
-        'username'  => $username,
-        'password'  => $password,
-        'charset'   => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix'    => '',
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
         ],
 
         'pgsql' => [
